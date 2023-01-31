a5d55b332765a2410184c104206205458a4c67bc
Ace <kronicrespawn@gmail.com>
2023-01-30 20:01:55 -0600

    roadmapping functions

--- factories/shipFactory.js
+++ factories/shipFactory.js
@@ -2,13 +2,33 @@ const shipType = (name, length) => {
     const health = length;
     const type  = name;
 
+  const hit = () => {
+//Ships should have a hit() function that 
+//increases the number of ‘hits’ in your ship.
+//when enemy attacks and hit, increase hit damage on ship 
+//by 1
+  }  
+
+
+
+  const ifSunk = () => {
+    if (health == 0) {
+    //for each hit accumulated, decrease health by 1
+    //when health reaches zero, "you sunk my battle ship"
+    //ship is destroyed
+    } else {
+
+    }
+    }
+
 return {health, type}
 }
 
-// const cruiser = shipType("cruiser", 3);
 
-// console.log(cruiser);
 module.exports = {shipType}
+
+//isSunk() should be a function that calculates it based on 
+//their length and the number of ‘hits’.
 
 
 //rules for battleshoip
