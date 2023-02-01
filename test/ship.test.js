07e7c4bdec89d289c703003f1907a03de49eaf08
Ace <kronicrespawn@gmail.com>
2023-01-31 20:33:03 -0600

    test added

--- test/ship.test.js
+++ test/ship.test.js
@@ -2,6 +2,9 @@
 const ship = require("../factories/shipFactory");
 describe("Ship's Desription", () => {
 test('check health', () => {
-    expect(ship.shipType("ted", 5)).toEqual({"health": 5, "type": "ted"});
+    expect(ship.ship("ted", 5)).toEqual({"health": 5, "type": "ted"});
   });
+  test('check if hit', () => {
+    expect(ship.hit("X")).toMatch("enemy hit");
+  })
 })
