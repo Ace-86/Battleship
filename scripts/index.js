48df7203c05c0099bacf9b3936aee81d8048e684
Ace <kronicrespawn@gmail.com>
2023-02-01 20:48:40 -0600

    board creation added

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,16 @@
 let myGridSize = 5
 let enemyGridSize = 5
 let myGrid = createGrid(myGridSize);
-let enemyGrid = creatorGrid(enemyGridSize)
+let enemyGrid = createGrid(enemyGridSize)
 
-
+printGrid(enemyGrid, true)
+printGrid(myGrid)
 // use an array of an array for board creation
 
 function createGrid(size) {
     let grid = [];
     for (let i = 0; i < size; i++) {
-        grid[i] = [j];
+        grid[i] = [];
         for (let j = 0; j < size; j++) {    
             grid[i][j] = "-";
         }
@@ -20,14 +21,16 @@ function createGrid(size) {
 function printGrid(grid, isEnemy = false) {
     const headers = createHeaders(grid.length);
     console.log(headers);
-    for (let i = 0; i < grid.length ; i++)
-    rowString= i  + ' ';
-    for (let cell of grid[i]) {
-        if( isEnemy && cell == "O") {
-            rowString += '- ';
-        } else {
-            rowString += cell + " ";
+    for (let i = 0; i < grid.length ; i++) {
+        let rowString = i  + ' ';
+        for (let cell of grid[i]) {
+            if( isEnemy && cell == "O") {
+                rowString += '- ';
+            } else {
+                rowString += cell + "  ";
+            }   
         }
+        console.log(rowString)
     }
 }
 
@@ -39,11 +42,11 @@ function createHeaders(size) {
     return result;
 }
 
-function createRowHeaders(size) {
-    let result = "  ";
-    for (let j = 0; j < size; j++) {
-        result += i  +  '  ';
-    }
-    return result;
-}
+// function createRowHeaders(size) {
+//     let result = "  ";
+//     for (let j = 0; j < size; j++) {
+//         result += i  +  '  ';
+//     }
+//     return result;
+// }
 
