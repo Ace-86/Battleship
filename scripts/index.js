c12833d935e6ba9290611b9e2b6179f8701c93ad
Ace <kronicrespawn@gmail.com>
2023-02-02 09:01:08 -0600

    random enemy placement functional

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ let enemyGrid = createGrid(enemyGridSize)
 //gameplay variables
 let myShips = 5;
 let enemyShips = 5;
-
+let enemyLocation = {}
 
 // printGrid(enemyGrid, true)
 // printGrid(myGrid)
@@ -16,10 +16,10 @@ for (let i = 1; i < 6; i++) {
     let x = prompt('enter y coordinate for your ship number' + i);
     let y = prompt('enter x coordinate for your ship number' + i);
     placeCharacter(x, y, 'O', myGrid);
+    placeRandomCharacter('O', enemyGrid, enemyGridSize)    
+    printGrid(enemyGrid, true)
+    printGrid(myGrid)
 }
-
-printGrid(enemyGrid, true)
-printGrid(myGrid)
 
 // use an array of an array for board creation
 
@@ -58,14 +58,28 @@ function createHeaders(size) {
     return result;
 }
 
-// function createRowHeaders(size) {
-//     let result = "  ";
-//     for (let j = 0; j < size; j++) {
-//         result += i  +  '  ';
-//     }
-//     return result;
-// }
 
+// place ship (GAMEPLAY)
 function placeCharacter( x, y, c, grid) {
     grid[y][x] = c;
 }
+
+function palceRandomCharacter(c , grid, max) {
+    //dont place ship in same location
+    let didPlace = false;
+    while(!didPlace) {
+        let x = getRandomInt(max);
+        let y = getRandomInt(min)
+        if (!enemyLocation[`${x}-${y}`]) {
+        placeCharacter(x, y, c, grid);
+        didPlace = true;
+        enemyLocation[`${x}-${y}`] = true;
+        }
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+
+
