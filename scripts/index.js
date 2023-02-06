1466a877c417bb9eee6a131e55be70e765f346df
Ace <kronicrespawn@gmail.com>
2023-02-06 11:22:32 -0600

    seperated functions to individual modules

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,12 @@
-//gameboard
+import {myGridSize, myGrid, enemyGridSize, enemyGrid } from './variables.js';
+import { printGrid } from './gameboard.js'
+import {placeRandomCharacter, getRandomInt} from './enemy.js'
 
-let myGridSize = 3
-let enemyGridSize = 3
-let myGrid = createGrid(myGridSize);
-let enemyGrid = createGrid(enemyGridSize)
 
-//gameplay variables
+// //gameplay variables
 let myShips = 2;
 let enemyShips = 2;
-let enemyLocation = {}
+// let enemyLocation = {}
 
 
 //Players place ships; player 1 selects location of ships; enemy uses random select to place character
@@ -16,11 +14,10 @@ for (let i = 1; i < 3; i++) {
     let x = prompt('enter y coordinate for your ship number' + i);
     let y = prompt('enter x coordinate for your ship number' + i);
     placeCharacter(x, y, 'O', myGrid);
-    placeRandomCharacter('O', enemyGrid, enemyGridSize)    
-    printGrid(enemyGrid, true)
-    printGrid(myGrid)
+    placeRandomCharacter('O', enemyGrid, enemyGridSize);    
+    printGrid(enemyGrid, true);
+    printGrid(myGrid);
 }
-
 
 //while loop will continue until a players ships = 0/ main gameplay loop
 while (enemyShips > 0 && myShips > 0) {
@@ -46,7 +43,6 @@ while (enemyShips > 0 && myShips > 0) {
         printGrid(myGrid);
 }
 
-
 //Determines winner, prints win or lose
 if (myShips < enemyShips) {
     console.log('Lose')
@@ -54,71 +50,9 @@ if (myShips < enemyShips) {
     console.log('victory')
 }
 
-// array of an array for board creation
-function createGrid(size) {
-    let grid = [];
-    for (let i = 0; i < size; i++) {
-        grid[i] = [];
-        for (let j = 0; j < size; j++) {    
-            grid[i][j] = "-";
-        }
-    }
-    return grid;
-}
-
-
-//will print gameboard grid for both players
-function printGrid(grid, isEnemy = false) {
-    const headers = createHeaders(grid.length);
-    console.log(headers);
-    for (let i = 0; i < grid.length ; i++) {
-        let rowString = i  + ' ';
-        for (let cell of grid[i]) {
-            if( isEnemy && cell == "O") {
-                rowString += '- ';
-            } else {
-                rowString += cell + "  ";
-            }   
-        }
-        console.log(rowString)
-    }
-}
-
-//create board headers that allow easier time picking coordinates; shows numbered rows and columns
-function createHeaders(size) {
-    let result = "  ";
-    for (let i = 0; i < size; i++) {
-        result += i  +  '  ';
-    }
-    return result;
-}
-
-
-// place ship (GAMEPLAY)
+// // place ship (GAMEPLAY)
 function placeCharacter( x, y, c, grid) {
     grid[y][x] = c;
-}
-
-
-//enemy placing random ships, coordinates are randomized from getRandomInt
-function placeRandomCharacter(c , grid, max) {
-    //prevent enemy from placing ship in same location
-    let didPlace = false;
-    while(!didPlace) {
-        let x = getRandomInt(max);
-        let y = getRandomInt(max)
-        if (!enemyLocation[`${x}-${y}`]) {
-        placeCharacter(x, y, c, grid);
-        didPlace = true;
-        enemyLocation[`${x}-${y}`] = true;
-        }
-    }
-}
-
-//gameplay function allowing enemy to place ships and attack randomly
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max))
 }
 
 // attack function for player 1
@@ -140,3 +74,5 @@ function attack(x, y, grid) {
 function drawbreak() {
     console.log('------------------------------------')
 }
+
+export {placeCharacter}
