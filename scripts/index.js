d9e7dfeafa7c57aa54127f16d5af3e361ffd8c75
Ace <kronicrespawn@gmail.com>
2023-02-06 11:48:48 -0600

    remove comment; added import

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,12 @@
-import {myGridSize, myGrid, enemyGridSize, enemyGrid } from './variables.js';
+import { myGridSize, myGrid, enemyGridSize, enemyGrid } from './variables.js';
 import { printGrid } from './gameboard.js'
 import { placeCharacter } from './player.js';
-import {placeRandomCharacter, getRandomInt} from './enemy.js'
-
+import { placeRandomCharacter, getRandomInt } from './enemy.js'
+import { attack } from './attack.js'
 
 // //gameplay variables
 let myShips = 2;
 let enemyShips = 2;
-// let enemyLocation = {}
-
 
 //Players place ships; player 1 selects location of ships; enemy uses random select to place character
 for (let i = 1; i < 3; i++) {
@@ -25,19 +23,16 @@ while (enemyShips > 0 && myShips > 0) {
     //ask player 1 to enter coordinates on board for attack; 
     let x = prompt('Enter the x coordinate for your attack'); //local variables
     let y = prompt('Enter the y coordinate for your attack'); //local variables
-
     //player 1 attack turn
         if(attack(x, y, enemyGrid)) {
             enemyShips--; //if hit, decrease enemy ships by 1
         } 
-
     //enemy attack turn/ picks random coordinates 
             x = getRandomInt(myGridSize); 
             y = getRandomInt(myGridSize);
         if (enemyShips > 0 && attack(x, y, myGrid)) { //if enemy still has ships and attack lands
             myShips--; //decrease player 1 ships by 1
         }
-        
         drawbreak();   //visual shows the break between board changes every move     
         //prints updated grid
         printGrid(enemyGrid, true);
@@ -49,21 +44,6 @@ if (myShips < enemyShips) {
     console.log('Lose')
 } else {
     console.log('victory')
-}
-
-// attack function for player 1
-function attack(x, y, grid) {
-    if ( grid[y][x] == 'O') {
-        grid[y][x] = '!';
-        return true;
-        // attack function returns a boolean
-    } else if (grid[y][x] == '-') {
-        grid[y][x] = 'x';
-        return false;
-        //no hit; missed attack
-    }else {
-        return false//no new ship has been hit
-    }
 }
 
 // visual break after each board change instance
