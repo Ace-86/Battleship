f65f9fd562b9ac5acb1c7da3ef7a1a101a57b587
Ace <kronicrespawn@gmail.com>
2023-02-07 20:51:40 -0600

    test added; not finalized

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,9 +1,19 @@
 import { printGrid } from "../scripts/gameboard";
+import { placeCharacter } from "../scripts/player";
 
 test('returns empty gameboard after creation', () => {
     expect(printGrid).toEqual([
       ['-', '-', '-'],
       ['-', '-', '-'],
+      ['-', '-', '-'],
+    ]);
+  });
+
+  test('allows placing ship', () => {
+    const board = printGrid(3);
+    expect(placeCharacter(1,1)).toEqual([
+      ['-', '-', '-'],
+      ['-', 'O', '-'],
       ['-', '-', '-'],
     ]);
   });
