751808eb417aa72978481068de06cd6918f2ad4a
Ace <kronicrespawn@gmail.com>
2023-02-07 20:44:22 -0600

    test updated; not finalized

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,10 +1,22 @@
 
-const ship = require("../factories/shipFactory");
-describe("Ship's Desription", () => {
-test('check health', () => {
-    expect(ship.ship("ted", 5)).toEqual({"health": 5, "type": "ted"});
-  });
-  test('check if hit', () => {
-    expect(ship.hit("X")).toMatch("enemy hit");
-  })
-})
+test('returns Battleship object', () => {
+  expect(new Battleship(3, [0, 0], false).getParts).toEqual([false, false, false]);
+});
+
+test('can be hit', () => {
+  const battleship = new Battleship(3, [0, 0], false);
+  battleship.hit(2);
+  expect(battleship.getParts).toEqual([false, false, true]);
+});
+
+test("can't choose hit value heigher than length - 1", () => {
+  expect(() => new Battleship(3, [0, 0], false).hit(3)).toThrow();
+});
+
+test('isSunk returns correct value if ship is sunk', () => {
+  const battleship = new Battleship(3, [0, 0], false);
+  battleship.hit(0);
+  battleship.hit(1);
+  battleship.hit(2);
+  expect(battleship.isSunk()).toBe(true);
+});
